10bfc3a52193441b9ee3cdad7e55e33f422c77c3
vinit-jpl <vinit.jpl@gmail.com>
2025-01-02 17:00:55 +0530

    handling post request in go

--- 21webverbs/main.go
+++ 21webverbs/main.go
@@ -9,27 +9,55 @@ import (
 
 func main() {
 	fmt.Println("Welcome to webverbs")
-	PerformGetRequest()
+	// PerformGetRequest()
+	PerformPostRequest()
 }
 
-func PerformGetRequest() {
-	const myurl = "http://localhost:3000/get"
-	response, err := http.Get(myurl)
+// func PerformGetRequest() {
+// 	const myurl = "http://localhost:3000/get"
+// 	response, err := http.Get(myurl)
+
+// 	if err != nil {
+// 		panic(err)
+
+// 	}
+
+// 	defer response.Body.Close()
+// 	fmt.Println("Status code: ", response.StatusCode)
+// 	fmt.Println("Content length is:", response.ContentLength)
+
+// 	var responseString strings.Builder
+// 	content, _ := io.ReadAll(response.Body)
+// 	byteCount, _ := responseString.Write(content)
+
+// 	fmt.Println("Bytecount is: ", byteCount)
+// 	fmt.Println(responseString.String())
+// 	// fmt.Println(string(content))
+// }
+
+func PerformPostRequest() {
+	const myurl = "http://localhost:3000/post"
+
+	// fake json payload
+
+	requestBody := strings.NewReader(`
+		{
+	    	"coursename": "Lest's go with Go Lang",
+			"price": 0,
+			"platform": "learncodeonline.in"
+
+		}
+
+	`)
+	response, err := http.Post(myurl, "application/json", requestBody)
 
 	if err != nil {
 		panic(err)
-
 	}
 
 	defer response.Body.Close()
-	fmt.Println("Status code: ", response.StatusCode)
-	fmt.Println("Content length is:", response.ContentLength)
 
-	var responseString strings.Builder
 	content, _ := io.ReadAll(response.Body)
-	byteCount, _ := responseString.Write(content)
 
-	fmt.Println("Bytecount is: ", byteCount)
-	fmt.Println(responseString.String())
-	// fmt.Println(string(content))
+	fmt.Println(string(content))
 }
