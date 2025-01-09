a6ded63df1a3fddd6ae8a7d778ba96289d12697f
vinit-jpl <vinit.jpl@gmail.com>
2025-01-09 19:02:18 +0530

    creating json data in go lang

---
+++ 22bitmorejson/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type course struct {
+	Name     string `json:"coursename"`
+	Price    int
+	Platform string   `json:"website"`
+	Password string   `json:"-"`
+	Tags     []string `json:"tags,omitempty"` //be careful of space wile using omitempty
+}
+
+func main() {
+	fmt.Println("Welcome to json class")
+	EncodeJson()
+}
+
+func EncodeJson() {
+	lcoCourses := []course{
+		{"ReactJs Bootcamp", 299, "LearnCodeOnline.in", "abc123", []string{"web-dev", "js"}},
+		{"MERN Bootcamp", 199, "LearnCodeOnline.in", "bcd123", []string{"full-stack", "js"}},
+		{"Angular Bootcamp", 299, "LearnCodeOnline.in", "vit123", nil},
+	} //slice of type courses
+
+	// package this data as JSON data
+
+	finalJson, err := json.MarshalIndent(lcoCourses, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%s\n", finalJson)
+}
---
+++ notes/notes.md
@@ -0,0 +1,1 @@
+![io/ioutil depraction resolved]({986B3C49-9B4E-4179-9712-2EE7D75A13A6}.png)
